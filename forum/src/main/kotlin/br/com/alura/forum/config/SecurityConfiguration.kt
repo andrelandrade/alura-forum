b190ff186466fc77b8b05c0e78615821592f06cc
André Luis de Andrade <al.andrade256@gmail.com>
2023-03-23 07:03:33 -0300

    Segurança com JWT

--- forum/src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ forum/src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -1,30 +1,41 @@
 package br.com.alura.forum.config
 
+import br.com.alura.forum.security.JWTAuthenticationFilter
+import br.com.alura.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration(
+    private val configuration: AuthenticationConfiguration,
+    private val jwtUtil: JWTUtil
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeHttpRequests()?.
+        http.
+            csrf()?.disable()?.
+            authorizeHttpRequests()?.
             requestMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
+            requestMatchers(HttpMethod.POST,"/login")?.permitAll()?.
             anyRequest()?.
             authenticated()?.
-            and()?.
-            sessionManagement()?.
-            sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
-            and()?.
-            formLogin()?.
-            disable()?.
-            httpBasic()
+            and()
+            http.addFilterBefore(JWTLoginFilter(authManager = configuration.authenticationManager, jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+            http.addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+            http.sessionManagement()?.
+            sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
         return http.build()
     }
 
