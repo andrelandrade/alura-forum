a90cf8ef5ec42b9c504b0d09de58084749dfba03
André Luis de Andrade <al.andrade256@gmail.com>
2023-03-27 06:49:43 -0300

    Testes manuais

--- forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 class TopicoService(
@@ -56,6 +57,7 @@ class TopicoService(
 
         topico.titulo = form.titulo
         topico.mensagem = form.mensagem
+        topico.dataAlteracao = LocalDate.now()
 
         return topicoViewMapper.map(topico)
     }
